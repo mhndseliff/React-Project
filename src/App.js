c3fa0013037b11bc48cae0d5fd05526cd448cd46
mhndseliff <69609886+mhndseliff@users.noreply.github.com>
2021-07-03 21:39:36 +0300

    React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+import{Container,Row,Col} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state={currenCategory:"",products:[]};
+
+
+  componentDidMount(){
+    this.getProducts();
+  }
+  
+ 
+ 
+   changeCategory = category =>{
+    this.setState({currenCategory:category.categoryName});
+    console.log(category);
+    this.getProducts(category.id);
+ };
+
+
+ getProducts=categoryId => {
+
+let url="http://localhost:3000/products";
+if(categoryId){
+  url+="?categoryId="+categoryId;
 }
 
-export default App;
+  fetch(url)
+  .then(response=>response.json())
+  .then(data=>this.setState({products:data}));
+}
+
+
+ 
+  render() {
+    let productInfo = {title:"ProductList",baskaBisey:"bisey"}
+    let categoryInfo = {title:"CategortyList"}
+
+
+    return (
+      <div>
+        <Container> 
+        <Navi /> 
+        
+        <Row>
+        <Col xs="3">
+        <CategoryList  changeCategory={this.changeCategory} info={categoryInfo}/>
+        </Col>
+        <Col xs="9">
+        <ProductList 
+        products={this.state.products}
+        currenCategory={this.state.currenCategory} 
+        info={productInfo}
+        />
+        </Col>
+      
+        </Row>
+        </Container>
+        
+      
+      
+      </div>
+    );
+  }
+
+
+
+}
+
